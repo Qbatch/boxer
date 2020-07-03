09ef3884d56063e3d9fd069939e165cfffa36c94
ali-qbatch <ali.haider@qbatch.com>
2020-07-03 13:04:26 +0500

    make mobile header

--- js/Header.js
+++ js/Header.js
@@ -5,4 +5,16 @@ $(document).ready(function () {
   $(document).on('click', ".lines-button.close", function () {
     $('.lines-button').removeClass('close');
   });
+  $('#logo').click(function () {
+    $(".header-section").toggleClass("header-color");
+  })
+
 });
+function changeImg() {
+  var logo = document.getElementById("menu-logo");
+  if (logo.getAttribute('src') == "./Assets/WebLogoBlue.png") {
+    logo.src = "./Assets/logo.png";
+  } else {
+    logo.src = "./Assets/WebLogoBlue.png";
+  }
+}
