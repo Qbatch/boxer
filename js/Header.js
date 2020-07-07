0cd908e373ed0ba971ab9ddca8e883d1aee0817a
ali-qbatch <ali.haider@qbatch.com>
2020-07-07 12:07:58 +0500

    Today update code

--- js/Header.js
+++ js/Header.js
@@ -1,23 +1,3 @@
-$(document).ready(function () {
-  $(document).on('click', ".lines-button", function () {
-    $('.lines-button').addClass('close');
-  });
-  $(document).on('click', ".lines-button.close", function () {
-    $('.lines-button').removeClass('close');
-  });
-  $('#logo').click(function () {
-    $(".overlay-div ").toggleClass("header-color");
-  })
-
-});
-function changeImg() {
-  var logo = document.getElementById("menu-logo");
-  if (logo.getAttribute('src') == "./Assets/WebLogoBlue.png") {
-    logo.src = "./Assets/logo.png";
-  } else {
-    logo.src = "./Assets/WebLogoBlue.png";
-  }
-}
 $(function () {
   $(window).scroll(function () {
     if ($(this).scrollTop() > 50) {
@@ -38,13 +18,10 @@ $(function () {
     }
   })
 });
-
 function openNav() {
   document.getElementById("mySidebar").style.width = "325px";
   document.getElementById("main").style.marginright = "325px";
 }
-
-/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginright = "0";
