fb891b350e2b2fb9e14bbb285e8d684fe19afc53
ali-qbatch <ali.haider@qbatch.com>
2020-07-09 19:21:05 +0500

    Today update code

--- js/Header.js
+++ js/Header.js
@@ -1,28 +1,26 @@
 $(function () {
   $(window).scroll(function () {
     if ($(this).scrollTop() > 50) {
-      $('#menu-logo').attr('src', './Assets/WebLogoBlue.png');
-      $('#main img').attr('src', './Assets/WebLogoBlue.png');
-      $('.openbtn').addClass('close-btn');
-      $('#main').addClass('active');
-      $('.navbar').addClass('active')
+      $("#menu-logo").attr("src", "./Assets/WebLogoBlue.png");
+      $("#main img").attr("src", "./Assets/WebLogoBlue.png");
+      $(".openbtn").addClass("close-btn");
+      $("#main").addClass("active");
+      $(".navbar").addClass("active");
     }
     if ($(this).scrollTop() < 50) {
-      $('#menu-logo').attr('src', './Assets/logo.png');
-      $('#main img').attr('src', './Assets/logo.png');
-      $('.openbtn').removeClass('close-btn');
-      $('#main').removeClass('active');
-      $('.navbar').removeClass('active')
-      console.log('hello')
-
+      $("#menu-logo").attr("src", "./Assets/logo.png");
+      $("#main img").attr("src", "./Assets/logo.png");
+      $(".openbtn").removeClass("close-btn");
+      $("#main").removeClass("active");
+      $(".navbar").removeClass("active");
     }
-  })
+  });
 });
-function openNav() {
+const openNav = () => {
   document.getElementById("mySidebar").style.width = "325px";
   document.getElementById("main").style.marginright = "325px";
-}
-function closeNav() {
+};
+const closeNav = () => {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginright = "0";
-}
+};
